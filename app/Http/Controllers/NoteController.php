d994d7c03e3ff8b616d1652d0ca88bff17b28b5c
Dvs149 <149dvs@gmail.com>
2023-03-10 05:43:30 +0530

    done some change while showing it to interviewier, now reverting it back

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -16,11 +16,15 @@ class NoteController extends Controller
      */
     public function index()
     {
-
-        $notes = Note::with('user')->where('user_id',Auth::user()->id)->orderBy('is_pin', 'DESC')->get();
-        $notes = User::with('note')->where('id',Auth::user()->id)->get();
-        dd($notes->toArray());
-        return view('notes.index',compact('notes'));
+        $notes = Note::with('user')
+            ->where('user_id', Auth::user()->id)
+            ->orderBy('is_pin', 'DESC')
+            ->get();
+        // $notes = User::with('note')
+        //     ->where('id', Auth::user()->id)
+        //     ->get();
+        // dd($notes->toArray());
+        return view('notes.index', compact('notes'));
     }
 
     /**
@@ -41,10 +45,10 @@ class NoteController extends Controller
         $message = 'updated';
 
         $id = $request->id;
-        $notes =new Note;
+        $notes = new \Note();
 
-        if($id != ''){
-            $notes=Note::FindOrFail($id);
+        if ($id != '') {
+            $notes = \Note::FindOrFail($id);
         }
         if ($id == null) {
             $message = config('custom.ADDED_SUCCESFULL');
@@ -55,14 +59,14 @@ class NoteController extends Controller
         // $notes->save();
         //store the image if requested
         if (request()->hasFile('image')) {
-            if(!empty($note->image)){
-                $file= $note->image;
-                @unlink(config('custom.PATH').$file);
+            if (!empty($note->image)) {
+                $file = $note->image;
+                @unlink(config('custom.PATH') . $file);
             }
             $file = request()->file('image');
-            $fileName = md5($file->getClientOriginalName() . time()) . "." . $file->getClientOriginalExtension();
+            $fileName = md5($file->getClientOriginalName() . time()) . '.' . $file->getClientOriginalExtension();
             $file->move(config('custom.PATH'), $fileName);
-            $notes->image=$fileName;
+            $notes->image = $fileName;
         }
         $notes->save();
         return redirect(route('notes.index'))->with('message', $message);
@@ -71,7 +75,7 @@ class NoteController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Note $note)
+    public function show(\Note $note)
     {
         //
     }
@@ -79,17 +83,17 @@ class NoteController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Note $note)
+    public function edit(\Note $note)
     {
         // dd($note->toArray());
 
-        return view('notes.edit',compact('note'));
+        return view('notes.edit', compact('note'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateNoteRequest $request, Note $note)
+    public function update(UpdateNoteRequest $request, \Note $note)
     {
         //
     }
@@ -100,21 +104,21 @@ class NoteController extends Controller
     public function destroy($id)
     {
         // dd($note->toArray());
-        
-        $note=Note::FindOrFail($id);
-        $file= $note->image;
-        $filename = storage_path(config('custom.PATH')).$file;
-        
-        if(file_exists($filename)){
+
+        $note = \Note::FindOrFail($id);
+        $file = $note->image;
+        $filename = storage_path(config('custom.PATH')) . $file;
+
+        if (file_exists($filename)) {
             @unlink($filename);
         }
         $note->delete();
-        return "delete";
+        return 'delete';
     }
     public function pin($id)
     {
         // dd($id);
-        $note = Note::find($id);
+        $note = \Note::find($id);
         $note->is_pin = 1;
         $note->save();
         return true;
@@ -123,7 +127,7 @@ class NoteController extends Controller
     public function unpin($id)
     {
         // dd($id);
-        $note = Note::find($id);
+        $note = \Note::find($id);
         $note->is_pin = null;
         $note->save();
         return true;
